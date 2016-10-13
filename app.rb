4c2b7586ee0bc58729d0ed34323ba0ebaa22244b
Jess <jess.harrelson0707@gmail.com>
2016-10-13 19:29:47 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    @name = "My name is Jess"
+    #erb: 'name/index.html.erb'
+  end
+
+  get '/hometown' do
+    @name = "My hometown is Sheridan"
+    #erb: 'name/index.html.erb'
+  end
+
+  get '/favorite-song' do
+    @name = "My favorite song is this"
+    #erb: 'name/index.html.erb'
+  end
+
+
 end
